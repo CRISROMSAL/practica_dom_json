b395b768f55c135e6f8fcf8f41dd5cad3aacb752
Cristina Roman <cristinaroman.24@campuscamara.es>
2025-10-23 09:11:28 +0200

    Ejercico 2

---
+++ script.js
@@ -0,0 +1,12 @@
+// Ejercico 1 Cambiar el texto del h1 usando getElementById
+document.getElementById('titulo').textContent = 'Nuevo título';
+
+// Ejercicoi 1 Cambiar el color del párrafo usando getElementsByTagName
+document.getElementsByTagName('p')[0].style.color = 'pink'; //El [0] es para seleccionar el primer párrafo.
+
+// Ejercico 2 Cambiar el fondo del div usando getElementsByClassName
+const textos = document.getElementsByClassName('texto');
+
+for (let i = 0; i < textos.length; i++) {
+    textos[i].style.backgroundColor = 'gray';
+}
